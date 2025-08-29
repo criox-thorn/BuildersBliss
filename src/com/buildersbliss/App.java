b9738151d02500d441e0037b5984e83881ee7587
criox-thorn <criox.thorn@gmail.com>
2025-08-29 23:14:49 +1000

    Built Block Matching Capability

--- src/com/buildersbliss/App.java
+++ src/com/buildersbliss/App.java
@@ -2,14 +2,16 @@ package com.buildersbliss;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Properties;
+import java.util.*;
 import java.io.FileInputStream;
+import java.util.Arrays;
 
+import com.buildersbliss.io.BlockMatcher;
 import com.buildersbliss.io.JarExtractor;
 import com.buildersbliss.model.ColourUtils;
 import static com.buildersbliss.core.GradientGenerator.gradient;
+import static com.buildersbliss.io.BlockMatcher.match;
+import static com.buildersbliss.io.JarExtractor.createJSON;
 import static com.buildersbliss.model.ColourUtils.*;
 
 
@@ -26,18 +28,28 @@ public class App {
             System.err.println("Error loading settings file.");
         }
 
+        String jsonPath = "blocks.json";
+
         try {
             JarExtractor.extract("chipped-neoforge-1.21.1-4.0.2.jar", extFile);
+            jsonPath = createJSON();
 
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Error Extracting Jar");
         }
 
-        ArrayList gradientTest = gradient(new double[]{63,46,35},new double[]{11,30,-23},6);
 
 
-        System.out.println(deltaE2000(randomColourGenerator(), randomColourGenerator()));
+        ArrayList<double[]> gradientTest = gradient(new double[]{63,46,35},new double[]{11,30,-23},6);
+
+        for (double[] arr : gradientTest) {
+            System.out.println(Arrays.toString(arr));
+        }
+
+        String[] colourMatch = match(new double[]{63.0, 46.0, 35.0}, jsonPath, 10);
+
+        System.out.println(Arrays.toString(colourMatch));
 
     }
 }
--- src/com/buildersbliss/core/GradientGenerator.java
+++ src/com/buildersbliss/core/GradientGenerator.java
@@ -1,11 +1,11 @@
 package com.buildersbliss.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GradientGenerator {
     public static ArrayList<double[]> gradient(double[] start, double[] end, int steps) {
         ArrayList<double[]> gradient = new ArrayList<>();
-        gradient.add(start);
 
         for (int i = 0; i < steps; i++) {
             double t = (double) i / (steps - 1); // interpolation factor 0 → 1
@@ -15,7 +15,13 @@ public class GradientGenerator {
 
             gradient.add(new double[]{L,a,b});
         }
-        gradient.add(end);
         return gradient;
+    }
+
+    public static ArrayList<String> blockMatching(ArrayList<double[]> gradient) {
+        for (double[] colour : gradient) {
+            System.out.println(Arrays.toString(colour));
+        }
+        return null;
     }
 }
